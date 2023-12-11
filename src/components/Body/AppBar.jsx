e6fdd641be117e1a724663c370a9e49633c06dd4
shira <shira.k4255@gmail.com>
2023-12-11 19:13:28 +0200

    update menu - get category from server

--- src/components/Body/AppBar.jsx
+++ src/components/Body/AppBar.jsx
@@ -7,136 +7,116 @@ import List from '@mui/joy/List';
 import ListItemButton from '@mui/joy/ListItemButton';
 import Typography from '@mui/joy/Typography';
 import ModalClose from '@mui/joy/ModalClose';
-import Menu from '@mui/icons-material/Menu';
 import Search from '@mui/icons-material/Search';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 export default function MyMenu() {
   const [open, setOpen] = React.useState(false);
-  const nav = useNavigate()
+  const [categories, setCategories] = React.useState([]);
+  const nav = useNavigate();
+
+  React.useEffect(() => {
+    const fetchCategories = async () => {
+      try {
+        const response = await axios.get('http://localhost:8585/api/categories/getCategoris');
+        console.log('Response:', response);
+        setCategories(response.data);
+      } catch (error) {
+        console.error('Error fetching categories:', error);
+      }
+    };
+    
+    fetchCategories();
+  }, []);
+
   return (
     <>
-
-      
-        <React.Fragment>
-          <IconButton variant="outlined" color="neutral" onClick={() => setOpen(true)}>
-            <span class="material-symbols-outlined">
-              menu_open
-            </span>
-          </IconButton>
-          <Drawer open={open} onClose={() => setOpen(false)}>
-            <Box
-              sx={{
-                display: 'flex',
-                alignItems: 'center',
-                gap: 0.5,
-                ml: 'auto',
-                mt: 1,
-                mr: 2,
-              }}
+      <React.Fragment>
+        <IconButton variant="outlined" color="neutral" onClick={() => setOpen(true)}>
+          <span className="material-symbols-outlined">menu_open</span>
+        </IconButton>
+        <Drawer open={open} onClose={() => setOpen(false)}>
+          <Box
+            sx={{
+              display: 'flex',
+              alignItems: 'center',
+              gap: 0.5,
+              ml: 'auto',
+              mt: 1,
+              mr: 2,
+            }}
+          >
+            <Typography
+              component="label"
+              htmlFor="close-icon"
+              fontSize="sm"
+              fontWeight="lg"
+              sx={{ cursor: 'pointer' }}
             >
-              <Typography
-                component="label"
-                htmlFor="close-icon"
-                fontSize="sm"
-                fontWeight="lg"
-                sx={{ cursor: 'pointer' }}
-              >
-                Close
-              </Typography>
-              <ModalClose id="close-icon" sx={{ position: 'initial' }} />
-            </Box>
-            <Input
-              size="sm"
-              placeholder="Search"
-              variant="plain"
-              endDecorator={<Search />}
-              slotProps={{
-                input: {
-                  'aria-label': 'Search anything',
-                },
-              }}
-              sx={{
-                m: 3,
+              Close
+            </Typography>
+            <ModalClose id="close-icon" sx={{ position: 'initial' }} />
+          </Box>
+          <Input
+            size="sm"
+            placeholder="Search"
+            variant="plain"
+            endDecorator={<Search />}
+            slotProps={{
+              input: {
+                'aria-label': 'Search anything',
+              },
+            }}
+            sx={{
+              m: 3,
+              borderRadius: 0,
+              borderBottom: '2px solid',
+              borderColor: 'neutral.outlinedBorder',
+              '&:hover': {
+                borderColor: 'neutral.outlinedHoverBorder',
+              },
+              '&::before': {
+                border: '1px solid var(--Input-focusedHighlight)',
+                transform: 'scaleX(0)',
+                left: 0,
+                right: 0,
+                bottom: '-2px',
+                top: 'unset',
+                transition: 'transform .15s cubic-bezier(0.1,0.9,0.2,1)',
                 borderRadius: 0,
-                borderBottom: '2px solid',
-                borderColor: 'neutral.outlinedBorder',
-                '&:hover': {
-                  borderColor: 'neutral.outlinedHoverBorder',
-                },
-                '&::before': {
-                  border: '1px solid var(--Input-focusedHighlight)',
-                  transform: 'scaleX(0)',
-                  left: 0,
-                  right: 0,
-                  bottom: '-2px',
-                  top: 'unset',
-                  transition: 'transform .15s cubic-bezier(0.1,0.9,0.2,1)',
-                  borderRadius: 0,
-                },
-                '&:focus-within::before': {
-                  transform: 'scaleX(1)',
-                },
-              }}
-            />
-            <List
-              size="lg"
-              component="nav"
-              sx={{
-                flex: 'none',
-                fontSize: 'xl',
-                '& > div': { justifyContent: 'center' },
-              }}
-            >
-              <Button variant="text" onClick={() => { nav('/HomePage') }}>
-                <span class="material-symbols-outlined">
-                  home
-                </span>
-                <ListItemButton>Home Page</ListItemButton>
+              },
+              '&:focus-within::before': {
+                transform: 'scaleX(1)',
+              },
+            }}
+          />
+          <List
+            size="lg"
+            component="nav"
+            sx={{
+              flex: 'none',
+              fontSize: 'xl',
+              '& > div': { justifyContent: 'center' },
+            }}
+          >
+            {categories.map((category) => (
+              <Button
+                key={category.id}
+                variant="text"
+                onClick={() => {
+                  nav(`/Cards/${category.id}`); // Assuming you have a route for each category
+                }}
+              >
+                <span className="material-symbols-outlined">{category.icon}</span>
+                <ListItemButton>{category.name}</ListItemButton>
               </Button>
-
-              <Button variant="text" onClick={() => { nav('/Cards') }}>
-                <span class="material-symbols-outlined">
-                  web_stories
-                </span>
-                <ListItemButton>Web Development</ListItemButton>
-              </Button>
-
-              <Button variant="text" onClick={() => { nav('/Cards') }}>
-              <span class="material-symbols-outlined">
-              monitoring 
-                </span>
-                <ListItemButton>Data Science & Analytics</ListItemButton>
-              </Button>
-
-              <Button variant="text" onClick={() => { nav('/Cards') }}>
-                <span class="material-symbols-outlined">
-                  draw
-                </span>
-                <ListItemButton>UI/UX Design</ListItemButton>
-              </Button>
-
-              <Button variant="text" onClick={() => { nav('/Cards') }}>
-              <span class="material-symbols-outlined">
-              sports_esports
-                </span>
-                <ListItemButton>Game Development</ListItemButton>
-              </Button>
-
-              <Button variant="text" onClick={() => { nav('/Cards') }}>
-              <span class="material-symbols-outlined">
-                  policy
-                </span>
-                <ListItemButton>Cybersecurity</ListItemButton>
-              </Button>
-            </List>
-          </Drawer>
-        </React.Fragment>
-
-    
-
+            ))}
+          </List>
+        </Drawer>
+      </React.Fragment>
     </>
   );
 }
