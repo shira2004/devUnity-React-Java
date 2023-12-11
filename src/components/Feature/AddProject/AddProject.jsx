99e80aa939425dc34ca29e8fb507ac542687307d
shira <shira.k4255@gmail.com>
2023-12-12 00:32:55 +0200

    add style for add project component

--- src/components/Feature/AddProject/AddProject.jsx
+++ src/components/Feature/AddProject/AddProject.jsx
@@ -1,79 +1,87 @@
-import { useContext, useRef, useState } from 'react';
+import React from 'react';
+import { Grid, TextField, TextareaAutosize, Button } from '@mui/material';
+import './AddProject.css';
+import images from './image/images';
+import Header from '../../Header/Header';
+import CssBaseline from '@mui/material/CssBaseline';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 
-
-import images from '../assets/images.js';
+const theme = createTheme();
 
 export default function NewChallenge({ onDone }) {
-  const title = useRef();
-  const description = useRef();
-  const deadline = useRef();
-
-  const [selectedImage, setSelectedImage] = useState(null);
-  const { addChallenge } = useContext(ChallengesContext);
-
-  function handleSelectImage(image) {
-    setSelectedImage(image);
-  }
-
-  function handleSubmit(event) {
-    event.preventDefault();
-    const challenge = {
-      title: title.current.value,
-      description: description.current.value,
-      deadline: deadline.current.value,
-      image: selectedImage,
-    };
-
-    if (
-      !challenge.title.trim() ||
-      !challenge.description.trim() ||
-      !challenge.deadline.trim() ||
-      !challenge.image
-    ) {
-      return;
-    }
-
-    onDone();
-    addChallenge(challenge);
-  }
-
   return (
-    
-      <form id="new-challenge" onSubmit={handleSubmit}>
-        <p>
-          <label htmlFor="title">Title</label>
-          <input ref={title} type="text" name="title" id="title" />
-        </p>
+    <> 
+      <Header />
 
-        <p>
-          <label htmlFor="description">Description</label>
-          <textarea ref={description} name="description" id="description" />
-        </p>
+      <ThemeProvider theme={theme}>
 
-        <p>
-          <label htmlFor="deadline">Deadline</label>
-          <input ref={deadline} type="date" name="deadline" id="deadline" />
-        </p>
+        <Container component="main" maxWidth="md">
+          <CssBaseline />
+          <Box
+            sx={{
+              marginTop: 8,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <Typography component="h1" variant="h5">
+              Add Project
+            </Typography>
+            <form id="new-challenge">
+              <Grid container spacing={3}>
+                <Grid item xs={12}>
+                  <TextField fullWidth label="Title" name="title" id="title" />
+                </Grid>
 
-        <ul id="new-challenge-images">
-          {images.map((image) => (
-            <li
-              key={image.alt}
-              onClick={() => handleSelectImage(image)}
-              className={selectedImage === image ? 'selected' : undefined}
-            >
-              <img {...image} />
-            </li>
-          ))}
-        </ul>
+                <Grid item xs={12}>
+                  <TextareaAutosize
+                    fullWidth
+                    minRows={3}
+                    placeholder="Description"
+                    name="description"
+                    id="description"
+                  />
+                </Grid>
 
-        <p className="new-challenge-actions">
-          <button type="button" onClick={onDone}>
-            Cancel
-          </button>
-          <button>Add Challenge</button>
-        </p>
-      </form>
-   
+                <Grid item xs={12}>
+                  <TextareaAutosize
+                    fullWidth
+                    minRows={3}
+                    placeholder="where you want help"
+                    name="description"
+                    id="description"
+                  />
+                </Grid>
+
+
+                <Grid item xs={12}>
+                  <TextField fullWidth type="date" label="Deadline" name="deadline" id="deadline" />
+                </Grid>
+
+                <Grid item xs={12}>
+                  <ul id="new-challenge-images">
+                    {images.map((image) => (
+                      <li key={image.alt}>
+                        <img {...image} />
+                      </li>
+                    ))}
+                  </ul>
+                </Grid>
+
+                <Grid item xs={12}>
+                  <Button variant="contained" color="primary" onClick={onDone}>
+                    Add Challenge
+                  </Button>
+                </Grid>
+              </Grid>
+            </form>
+          </Box>
+        </Container>
+      </ThemeProvider>
+    </>
   );
 }
