10040116971483cbce80522ccf3a70c3640e8015
shira <shira.k4255@gmail.com>
2023-12-21 01:53:47 +0200

    send data - put to create new user

--- react/src/components/Pages/sign/SignIn.jsx
+++ react/src/components/Pages/sign/SignIn.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
